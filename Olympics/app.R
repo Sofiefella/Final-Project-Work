18804cbe1f231927e004d7e513a5658f173ad468
ollygill <ogill@college.harvard.edu>
2019-11-19 00:57:36 +0000

    almost done with the rough draft!

--- Olympics/app.R
+++ Olympics/app.R
@@ -13,124 +13,227 @@ library(plotly)
 library(shinythemes)
 
 all_data <- left_join(athlete_events, GDP, by = c("Team"))
-winter_medals <- read_csv("winter.csv")
-summer_medals <- read_csv("summer.csv")
-noc_regions <- read_csv("noc_regions.csv")
+winter_medals <- read_csv("winter.csv", col_types = cols(
+  Year = col_double(),
+  City = col_character(),
+  Sport = col_character(),
+  Discipline = col_character(),
+  Athlete = col_character(),
+  Country = col_character(),
+  Gender = col_character(),
+  Event = col_character(),
+  Medal = col_character()
+))
+summer_medals <- read_csv("summer.csv", col_types = cols(
+  Year = col_double(),
+  City = col_character(),
+  Sport = col_character(),
+  Discipline = col_character(),
+  Athlete = col_character(),
+  Country = col_character(),
+  Gender = col_character(),
+  Event = col_character(),
+  Medal = col_character()
+))
+noc_regions <- read_csv("noc_regions.csv", col_types = cols(
+  NOC = col_character(),
+  region = col_character(),
+  notes = col_character()
+))
 
 
 ui <- fluidPage(theme = shinytheme("superhero"),
                 
-                br(),
+      br(),
                 
-                navbarPage("The History of the Olympic Games",
+      navbarPage("The History of the Olympic Games",
                            
-                           tabPanel("Olympic Characteristics",
-                                    tabsetPanel(
+                tabPanel("Olympic Characteristics",
+                                    
+              tabsetPanel(
                                         
-                                        tabPanel("Winter Games", 
-                                                 h3("What Kind Of Trends Are Shown In The Winter Games?"),
-                                                 br(),
-                                                 sidebarPanel(
-                                                     selectInput("Medals")
-                                                 ),
-                                                 mainPanel(
-                                                     plotlyOutput("wintermedalPlotly"),
-                                                     br()
-                                                 ),
-                                                 br()
-                                                 ),
+                  tabPanel("Winter Games", 
+                       
+                      h3("What Kind Of Trends Are Shown In The Winter Games?"),
+                       
+                      br(),
+                       
+                      sidebarPanel(
+                       
+                        h4("Medals")
+                       
+                      ),
+                      
+                      mainPanel(
+                        
+                        plotlyOutput("wintermedalPlotly"),
+                        
+                        br()
+                        
+                        ),
+                        
+                        br()
+                        
+                        ),
                                         
-                                        tabPanel("Summer Games",
-                                                 h3("What Kind Of Trends Are Shown In The Summer Games?"),
-                                                 br(),
-                                                 sidebarPanel(
-                                                     selectInput("Medals")
-                                                 ),
-                                                 mainPanel(
-                                                     plotlyOutput("summermedalPlotly"),
-                                                     br()
-                                                 ),
-                                                 br()
-                                                 ),
+                  tabPanel("Summer Games",
+                       
+                        h3("What Kind Of Trends Are Shown In The Summer Games?"),
+                           
+                        br(),
+                          
+                        sidebarPanel(
+                              
+                          h4("Medals")
+                             
+                        ),
+                            
+                        mainPanel(
+                              
+                          plotlyOutput("summermedalPlotly"),
+                            
+                        br()
+                             
+                        ),
+                             
+                        br()
+                            
+                        ),
                                         
-                                        tabPanel("Location",
-                                                 h3("Where Were Athletes Coming From Over Time?"),
-                                                 br(),
-                                                 sidebarPanel(
-                                                     selectInput("Countries")
-                                                 ),
-                                                 mainPanel(
-                                                     plotlyOutput("mapsPlotly"),
-                                                     br()
-                                                 ),
-                                                 br(),
-                                                 ),
+                  
+                  tabPanel("Location",
+                        
+                        h3("Where Were Athletes Coming From Over Time?"),
+                        
+                        br(),
+                        
+                        sidebarPanel(
+                            
+                          h4("Countries")
+                          
+                        ),
+                        
+                        mainPanel(
+                          
+                          plotlyOutput("mapsPlotly"),
+                          
+                          br()
+                        
+                        ),
+                        
+                        br()
+                        
+                        )
                                         
-                                        )),
-                           tabPanel("Athlete Characteristics",
-                                    tabsetPanel(
-                                        tabPanel("Gender",
-                                                 h3("What Have Gender Trends Been Over Time?"),
-                                                 br(),
-                                                 sidebarPanel(
-                                                     selectInput("Season")
-                                                 ),
-                                                 mainPanel(
-                                                     plotlyOutput("winterGender"),
-                                                     br(),
-                                                 ),
-                                                 br(),
-                                                 ),
+                        )
+              ),
+                           
+                tabPanel("Athlete Characteristics",
+                                    
+             tabsetPanel(
                                         
-                                        tabPanel("Height",
-                                                 h3("What Are Height Trends Like For Olympic Athletes?"),
-                                                 br(),
-                                                 sidebarPanel(
-                                                   selectInput("Height")
-                                                 ),
-                                                 mainPanel(
-                                                   plotlyOutput("heightPlotly"),
-                                                   br()
-                                                 ),
-                                                 br(),
-                                        ),
+                 tabPanel("Gender",
+                      
+                      h3("What Have Gender Trends Been Over Time?"),
+                      
+                      br(),
+                      
+                      sidebarPanel(
+                          
+                          h4("Season")
+                      
+                      ),
+                     
+                      mainPanel(
+                          
+                        plotlyOutput("winterGender"),
+                      
+                        br()
+                      
+                      ),
+                      
+                      br()
+                      
+                      ),
                                         
-                                        tabPanel("Weight",
-                                                 h3("What Are Weight Trends Like For Olympic Athletes?"),
-                                                 br(),
-                                                 sidebarPanel(
-                                                   selectInput("Weight")
-                                                 ),
-                                                 mainPanel(
-                                                   plotlyOutput("weightPlotly"),
-                                                   br()
-                                                 ),
-                                                 br(),
-                                        )
+                  tabPanel("Height",
+                      
+                      h3("What Are Height Trends Like For Olympic Athletes?"),
+                      
+                      br(),
+                      
+                      sidebarPanel(
+                         
+                          h4("Height")
+                      
+                      ),
+                      
+                      mainPanel(
+                        
+                          plotlyOutput("heightPlotly"),
+                          
+                          br()
+                      
+                      ),
+                      
+                      br()
+                      
+                      ),
+                                        
+                   tabPanel("Weight",
+                      
+                      h3("What Are Weight Trends Like For Olympic Athletes?"),
+                      
+                      br(),
+                      
+                      sidebarPanel(
+                           
+                          h4("Weight")
+                      
+                      ),
+                      
+                      mainPanel(
+                          
+                          plotlyOutput("weightPlotly"),
+                          
+                          br()
+                          
+                          ),
+                      
+                      br()
+                      
+                      )
                 
                                     )),
                            
-                           tabPanel("About",
-                                    mainPanel(
-                                        h2("Summary"),
-                                        h5("This dashboard gives you the option to explore data collected from both the summer and winter Olympic Games throughout the years of 1896 to 2016. You have the option to select and view particular data from a variety of categories, including particular locations, events, medals won, gender, and so much more."),
+                  tabPanel("About",
+                      
+                      mainPanel(
+                          
+                          h2("Summary"),
+                          
+                          h5("This dashboard gives you the option to explore data collected from both the summer and winter Olympic Games throughout the years of 1896 to 2016. You have the option to select and view particular data from a variety of categories, including particular locations, events, medals won, gender, and so much more."),
+                            
+                                      
+                          h2("The Data"),
+                          
+                          h5("For this project I colleted three separate data sets. All of the visualizations are based on these data sets. Part of the data is from", a("Sports Reference", href="www.sports-reference.com"), ", collected in May of 2018 and the other two data sets were provided by the IOC Research and Reference Service and published by The Guardian's Datablog."),
                                         
-                                        h2("The Data"),
-                                        h5("For this project I colleted three separate data sets. All of the visualizations are based on these data sets. Part of the data is from", a("Sports Reference", href="www.sports-reference.com"), ", collected in May of 2018 and the other two data sets were provided by the IOC Research and Reference Service and published by The Guardian's Datablog."),
+                          h5("One data set contains information for 270,960 different events ranging from the start of the Olympics in 1896 to the 2016 games hosted in Rio. Another data set contains 5,770 different medals won by all different athltes during the winter games, and the last data set contains 30,065 different medals won by all different athletes during the summer games."),
+                             
+                                     
+                          h2("About Me"),
+                          
+                          h5("I am a sophomore undergraduate student at Harvard concentrating in Economics and pursuing a secondary in Global Health and Health Policy. At Harvard, I play on the Women's Lacrosse Team and find myself getting involved in many different organizations in the athletic department. I have grown up my whole life loving sports and as a result I wanted to use this project as an opportunity to use my passion for data science and sports to explore the Olympic Games beyond what you see on television."),
                                         
-                                        h5("One data set contains information for 270,960 different events ranging from the start of the Olympics in 1896 to the 2016 games hosted in Rio. Another data set contains 5,770 different medals won by all different athltes during the winter games, and the last data set contains 30,065 different medals won by all different athletes during the summer games."),
+                          h5("Feel free to reach out and contact me at ogill@college.harvard.edu or connect with me on LinkedIn", a("HERE", href="https://www.linkedin.com/in/olly-gill-081899160/")),
                                         
-                                        h2("About Me"),
-                                        h5("I am a sophomore undergraduate student at Harvard concentrating in Economics and pursuing a secondary in Global Health and Health Policy. At Harvard, I play on the Women's Lacrosse Team and find myself getting involved in many different organizations in the athletic department. I have grown up my whole life loving sports and as a result I wanted to use this project as an opportunity to use my passion for data science and sports to explore the Olympic Games beyond what you see on television."),
+                          h5("The source code for this Shiny App can be found at my GitHub")
                                         
-                                        h5("Feel free to reach out and contact me at ogill@college.harvard.edu or connect with me on LinkedIn", a("HERE", href="https://www.linkedin.com/in/olly-gill-081899160/")),
-                                        
-                                        h5("The source code for this Shiny App can be found at my GitHub")
-                                        
-                                    ))
+                                    ))))
                                     
 
-))
+
 
 
 
